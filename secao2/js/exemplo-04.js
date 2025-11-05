d52a0be46c0a14bfc014ff5cabe68ba4f4b0315c
HelioAlmeida12 <helioalmeida98@outlook.com>
2025-11-05 20:32:31 -0300

    att

---
+++ secao2/js/exemplo-04.js
@@ -0,0 +1,11 @@
+/*Vamos Resolver um problema?
+
+Pedir para o usuário digitar 2 números e mostrar a média
+ */
+
+let n1 = Number(prompt("Digite um número: "))
+let n2 = Number(prompt("Digite outro número: "))
+let media  = (n1 + n2) / 2
+let soma = n1 + n2
+alert(`A soma é: ${soma}`)
+alert(`E a média da soma de ${n1} e ${n2} é: ${media} `)
---
+++ secao2/js/exemplo-05.js
@@ -0,0 +1,16 @@
+/*Vamos Resolver um problema?
+
+
+
+
+ */
+
+let n = 10
+n+= 5
+n*= 5
+n++
+alert("O resto da divisão de " + n + " por 6 é " + (n % 6))
+alert("O quadrado do resto da divisão de " + n + " por 6 é " + ((n % 6) ** 2 ))
+alert(n)
+n /= 4
+alert(n)
